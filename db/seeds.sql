1031ca7d6d8c49dd768ee5b498a7eb266781fcf9
Tobi Bailey <baileygtobi@gmail.com>
2020-05-11 11:13:42 -0400

    updating folders

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Baccon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Melt');
