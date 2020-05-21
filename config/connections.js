e25025dde995b03db6421ae17f4147ccd51c6309
Tobi Bailey <baileygtobi@gmail.com>
2020-05-21 18:36:52 -0400

    updating connection.js file

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,37 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+var mysql = require("mysql");
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+} connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
 });
-
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -16,6 +40,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
 // Export connection for our ORM to use.
 module.exports = connection;
