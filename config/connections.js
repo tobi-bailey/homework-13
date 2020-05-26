f128f9db4129d43c8642a3ad39569c603280f378
Tobi Bailey <baileygtobi@gmail.com>
2020-05-26 18:32:03 -0400

    updated connections file

--- config/connections.js
+++ config/connections.js
@@ -24,14 +24,15 @@ var mysql = require("mysql");
 var connection;
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-} connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "ksswctlxfmco8dso",
-  password: "un2cevpmwzq3v5yo",
-  database: "burgers_db"
-});
+} 
+// else {
+// } connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "ksswctlxfmco8dso",
+//   password: "un2cevpmwzq3v5yo",
+//   database: "burgers_db"
+// });
 // Make connection.
 connection.connect(function(err) {
   if (err) {
